71ad41544ea455d48554b46a90c26e5f55a11b2e
ghita <ghitakhaled14@gmail.com>
2025-01-15 14:48:03 +0100

    Module odoo

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Gestion des Commandes',
+    'version': '1.0',
+    'summary': 'Module de gestion des commandes client',
+    'category': 'Sales',
+    'author': 'Ghita KHALED',
+    'website': 'https://github.com/Ghitaakhaled9/gestion-Commandes',
+    'depends': ['base', 'sale'],
+    'data': [
+    'views/gestion_commandes_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
